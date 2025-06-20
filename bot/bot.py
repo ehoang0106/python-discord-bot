aa8fed82b7622d154863146389e2e63d37a84838
ehoang0106 <khoahoang1698@gmail.com>
2025-06-20 09:45:01 -0700

    done

--- bot/bot.py
+++ bot/bot.py
@@ -1,8 +1,23 @@
 import discord
 from discord.ext import commands, tasks
 from dotenv import dotenv_values
-import time
-from datetime import datetime
+
+load_dotenv = dotenv_values("./bot/.env")
+TOKEN = load_dotenv["DISCORD_TOKEN"]
+PREFIX = load_dotenv["DISCORD_PREFIX"]
+
+bot = commands.Bot(command_prefix=PREFIX, help_command=None, intents = discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+    await bot.change_presence(activity=discord.Game(name="-help"))
 
 
-config = dotenv_values(".env")
+  
+@bot.event
+async def on_command_error(ctx, error):
+  if isinstance(error, commands.CommandNotFound):
+    await ctx.send("❌ Invalid command. Please use **`-help`** for a list of available commands.")
+
+bot.run(TOKEN)
